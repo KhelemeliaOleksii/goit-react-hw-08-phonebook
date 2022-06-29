c096b8b04ed410cc98220fcc04306c694951a86c
KhelemeliaOleksii <xvdm@ukr.net>
2022-06-29 20:07:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ import App from 'Components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <PersistGate loading={null} persistor={persistor}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </PersistGate>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </PersistGate>
+  </React.StrictMode>
 );
